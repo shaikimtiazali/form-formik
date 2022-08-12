5b551d5ef7f5dea293541be25b77314ed2f8fdeb
Imtiaz Shaik <imtiazali.ali36@gmail.com>
2022-08-12 13:37:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useFormik } from 'formik';
 import './App.css';
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: ''
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    validate: (values) => {
+      let errors = {};
+      if (!values.name) {
+        errors.name = "Name Required"
+      } if (!values.email) {
+        errors.email = "Email Required"
+      } if (!values.password) {
+        errors.password = "password Required"
+      }
+      return errors;
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={formik.handleSubmit} autoComplete='off'>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" value={formik.values.name} onChange={formik.handleChange} />
+        {formik.errors.name ? <div>{formik.errors.name}</div> : null}
+        <br />
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email" value={formik.values.email} onChange={formik.handleChange} />
+        {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+        <br />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" value={formik.values.password} onChange={formik.handleChange} />
+        {formik.errors.password ? <div>{formik.errors.password}</div> : null}
+        <br />
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 }
